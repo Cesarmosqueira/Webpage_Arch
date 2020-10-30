a92fa7893cfd9e0de54f7f6e46daf68b64dcd68d
cesarmosqueira <cesarmanuel.2802@gmail.com>
2020-10-30 18:24:42 -0500

    httprequest

--- submit.php
+++ submit.php
@@ -1,2 +1,7 @@
-<?php
-    echo 'Hello'. htmlspecialchars($_POST["username_input"] . '!');
+<?php 
+header("Content-Type: text/plain");
+echo "Hello dear {$_POST["username_input"]}!";
+echo '<script language="javascript">';
+echo 'alert(message successfully sent)';  //not showing an alert box.
+echo '</script>';
+?>
