52b5b340afbe56ab2ae71078dd82b7de04424865
Ubuntu <u201910750@ZS4A.skt2cjdnoiwebofashaysymjpe.bx.internal.cloudapp.net>
2020-10-30 23:57:20 +0000

    update

--- submit.php
+++ submit.php
@@ -1,6 +1,6 @@
 <?php 
 header("Content-Type: text/plain");
-echo "Hello dear {$_POST["username_input"]}!\n";
+echo "Hello dear {$_POST["Username"]}!\n";
 echo '<script> ';
 echo 'alert("message successfully sent")';  //not showing an alert box.
 echo '</script>';
