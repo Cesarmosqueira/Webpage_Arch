421dc482860f875de3b9b0ce76e16c25fb6f69d0
cesarmosqueira <cesarmanuel.2802@gmail.com>
2020-10-30 18:53:56 -0500

    update

--- submit.php
+++ submit.php
@@ -1,7 +1,7 @@
 <?php 
 header("Content-Type: text/plain");
-echo "Hello dear {$_POST["username_input"]}!";
-echo '<script language="javascript">';
-echo 'alert(message successfully sent)';  //not showing an alert box.
+echo "Hello dear {$_POST["username_input"]}!\n";
+echo '<script> ';
+echo 'alert("message successfully sent")';  //not showing an alert box.
 echo '</script>';
 ?>
