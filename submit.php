c429cae9de64f20d3bcad224479b85fddec35139
Ubuntu <u201910750@ZS4A.skt2cjdnoiwebofashaysymjpe.bx.internal.cloudapp.net>
2020-11-13 04:44:33 +0000

    Funcionaaaaaa gaaaaaa gaaaaa gaaaa

--- submit.php
+++ submit.php
@@ -1,22 +1,21 @@
-<META HTTP-EQUIV="REFRESH" CONTENT="2; URL=http://localhost/index.html">
 <?php
-$servidor = "localhost";
-$basededatos = "vgames";
-$usuario = "osproject";
-$password = "1234";
-
-$conexion = mysqli_connect($servidor, $usuario, $password, $basededatos);
-
-$Username = $_POST["Username"];
-$Email = $_POST["Email"];
-$Birthdate = $_POST["Birthdate"];
-
-if($Username==NULL or $Email==NULL or $Birthdate==NULL){}
-else {
-	$insertar= "INSERT INTO Users (Username, Email, Birthdate) VALUES ('$Username','$Email','$Birthdate')";
-	$query= mysqli_query($conexion,$insertar);
-}
-	header('Location: index.html');
-    die();
-    conexion->close();
+    $ip = "localhost";
+    $db = "vgames";
+    $dbuser = "osproject";
+    $dbpassword = "1234";
+    
+    $conn = mysqli_connect($ip, $dbuser, $dbpassword, $db);
+    if($conn->connect_error){
+        die("<b>Connection failed:</b><br>" .$conn->connect_error);       
+    }
+    $Username = $_POST["Username"];
+    $Email = $_POST["Email"];
+    $Birthdate = $_POST["Birthdate"];
+    if($Username==NULL or $Email==NULL or $Birthdate==NULL){}
+	else { 
+        $query= "INSERT INTO Users (Username, Email, Birthdate) VALUES ('$Username','$Email','$Birthdate')";
+	    mysqli_query($conn,$query);
+    }
+    mysqli_close($conn);
+    header("location: http://40.76.136.125/");
 ?>
