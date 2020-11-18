e4a69ebf6f23121985faf41aa5453503965ad893
cesarmosqueira <cesarmanuel.2802@gmail.com>
2020-11-17 23:25:48 -0500

    updqate

--- phpfiles/send_comment.php
+++ phpfiles/send_comment.php
@@ -1,10 +1,7 @@
 <?php
     $sqlconn = mysqli_connect("localhost", "osproject",  "1234", "vgames");
-    $query = 'UPDATE News set likes = likes+1 where code_index=' .$_POST['current'].' ' ;
+    $query = "INSERT INTO Comment (content, user, date, code_index) VALUES (" .$_POST["comment"]. ", SELENITA," .date("m-d-Y"). "," . $_POST["code_index"]. ")";
     $cons = mysqli_query($sqlconn, $query) or die ( "WTF!" );
     mysqli_close($sqlconn);
-    echo "Today is " . date("Y/m/d") . "<br>";
-    echo "Today is " . date("Y.m.d") . "<br>";
-    echo "Today is " . date("Y-m-d") . "<br>";
-    echo "Today is " . date("l");
+    header("Location: /")
 ?>
