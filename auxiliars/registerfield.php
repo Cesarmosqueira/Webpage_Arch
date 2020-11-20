7e6d4850cc7473bf988c548dc38b3a493441fc2a
Ubuntu <u201910750@ZS4A.skt2cjdnoiwebofashaysymjpe.bx.internal.cloudapp.net>
2020-11-20 00:23:17 +0000

    update

--- auxiliars/registerfield.php
+++ auxiliars/registerfield.php
@@ -6,7 +6,7 @@
             <div class="column">
                 <div class = "login">
                 <h3>Log In:</h3>
-                <form id ="loginform" action="validate_login.php">
+                <form id ="loginform" action="../phpfiles/validate_login.php">
                     <p><input type="text" name "username" placeholder="Username:"></p>
                     <p><input type="password" name "password" placeholder="Password:"></p>
                     <p><input type="submit" value="Log In!"></p>
@@ -16,7 +16,7 @@
             <div class="column">
                 <div class = "signup">
                 <h3>Sign Up:</h3>
-                <form id ="loginform" action="create_user.php">
+                <form id ="loginform" action="../phpfiles/create_user.php">
                     <p><input type="text" name "username" placeholder="Create Username:"></p>
                     <p><input type="password" name "password" placeholder="Create Password:"></p>
                     <p><input type="password" name "password" placeholder="Repeat Password:"></p>
