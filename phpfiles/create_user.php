27de5ddd673a104c3cb15cc3e6f02fac9455b64c
Ubuntu <u201910750@ZS4A.skt2cjdnoiwebofashaysymjpe.bx.internal.cloudapp.net>
2020-11-20 03:20:47 +0000

    register works

--- phpfiles/create_user.php
+++ phpfiles/create_user.php
@@ -1,34 +1,32 @@
 <?php
     $pass = $_POST["password1"];
-    $f = False;
+    $f = 0;
     if ($pass == $_POST["password2"]){
         $sqlconn = mysqli_connect("localhost", "osproject",  "1234", "vgames");
         $reps = mysqli_query($sqlconn, "SELECT * FROM Users where username='" .$_POST["username"]. "'");
         while($c = mysqli_fetch_array($reps)){
             echo "<script>alert('User already exists')</script>";
-            $f = True;
+            $f = 1;
         }
-        if(!$f){
+        if($f == 0){
             mysqli_query($sqlconn, "INSERT INTO Users VALUES('".$_POST["username"]."','".$pass."')");
         }
-        $mysqli_close($sqlconn);
+        mysqli_close($sqlconn);
     }
-    else{
+    else {
         echo "<script>alert('Passwords dont match')</script>";
         header("Location: /auxiliars/registerfield.html");
         die();
-
     }
-    if(!$f){
+    if($f == 0){
         echo "<script>alert('Registered Succesfully')</script>";
         header("Location: /");
         die();
     }
     else {
         echo "<script>alert('Error')</script>";
-        hader("Location: /auxiliars/registerfield.html");
-        e();
-
+        header("Location: /auxiliars/registerfield.html");
+        die();
     }
     header("Location: /");
 ?>
