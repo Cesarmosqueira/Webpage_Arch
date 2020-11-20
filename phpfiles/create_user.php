059b19a7dc647c0b7592ba6b44dc5e57897fb480
cesarmosqueira <cesarmanuel.2802@gmail.com>
2020-11-19 22:08:27 -0500

    update

--- phpfiles/create_user.php
+++ phpfiles/create_user.php
@@ -1,12 +1,12 @@
 <?php
     $pass = $_POST["password1"];
-    $f = false;
+    $f = False;
     if ($pass == $_POST["password2"]){
         $sqlconn = mysqli_connect("localhost", "osproject",  "1234", "vgames");
         $reps = mysqli_query($sqlconn, "SELECT * FROM Users where username='" .$_POST["username"]. "'");
         while($c = mysqli_fetch_array($reps)){
             echo "<script>alert('User already exists')</script>";
-            $f = true;
+            $f = True;
         }
         if(!$f){
             mysqli_query($sqlconn, "INSERT INTO Users VALUES('".$_POST["username"]."','".$pass."')");
@@ -14,20 +14,20 @@
         $mysqli_close($sqlconn);
     }
     else{
-        //echo "<script>alert('Passwords dont match')</script>";
-        //header("Location: /auxiliars/registerfield.html");
-        //die();
+        echo "<script>alert('Passwords dont match')</script>";
+        header("Location: /auxiliars/registerfield.html");
+        die();
 
     }
     if(!$f){
-        //echo "<script>alert('Registered Succesfully')</script>";
-        //header("Location: /");
-        //die();
+        echo "<script>alert('Registered Succesfully')</script>";
+        header("Location: /");
+        die();
     }
     else {
-        //echo "<script>alert('Error')</script>";
-        //header("Location: /auxiliars/registerfield.html");
-        //die();
+        echo "<script>alert('Error')</script>";
+        hader("Location: /auxiliars/registerfield.html");
+        e();
 
     }
     header("Location: /");
